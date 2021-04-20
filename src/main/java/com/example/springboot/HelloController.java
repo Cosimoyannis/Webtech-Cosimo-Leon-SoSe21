9d68c0d0175d81bb2f3b1158c970ef148b563073
Cosimoyannis <Cosimo.t@live.de>
2021-04-20 17:48:29 +0200

    HTML + CSS

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -1,12 +1,13 @@
 package com.example.springboot;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@RestController
+
+@Controller
 public class HelloController {
 
-    @RequestMapping(path = "/hello-world")
+    @GetMapping(path = "/hello-world")
     public String index() {
         return "helloworld";
     }
