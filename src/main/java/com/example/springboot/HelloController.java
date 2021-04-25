cedb682ce3cdda21d9308a0d7b8e7ec59fba2c15
Cosimoyannis <Cosimo.t@live.de>
2021-04-25 13:43:42 +0200

    Link geändert

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HelloController {
 
-    @GetMapping(path = "/hello-world")
+    @GetMapping(path = "/")
     public String index() {
         return "helloworld";
     }
