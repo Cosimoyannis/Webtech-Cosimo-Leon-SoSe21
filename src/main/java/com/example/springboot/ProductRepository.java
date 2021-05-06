9a784f8cb7c0babb2ad3aaa33e2c5bff378d607f
Cosimoyannis <Cosimo.t@live.de>
2021-05-06 14:08:57 +0200

    update all

--- src/main/java/com/example/springboot/ProductRepository.java
+++ src/main/java/com/example/springboot/ProductRepository.java
@@ -2,6 +2,8 @@ package com.example.springboot;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 
 @Repository
 public interface ProductRepository extends CrudRepository<Product, Long> {
