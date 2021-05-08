603a259784cb27c1d1ed0efcd6e6955842221db4
Cosimoyannis <Cosimo.t@live.de>
2021-05-08 17:05:22 +0200

    update

--- src/main/java/com/example/springboot/Product.java
+++ src/main/java/com/example/springboot/Product.java
@@ -6,6 +6,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.processing.Generated;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
