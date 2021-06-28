da66beccd9ef3f3d53ded9a4de0ae6766ac6c70b
Cosimoyannis <Cosimo.t@live.de>
2021-06-28 17:18:57 +0200

    HTML homepage und liste bearbeitet

--- src/test/java/com/demo/ApplicationTests.java
+++ src/test/java/com/demo/ApplicationTests.java
@@ -1,7 +1,6 @@
 package com.demo;
 import com.example.springboot.Product;
 import com.example.springboot.ProductRepository;
-import com.example.springboot.ProductService;
 import com.example.springboot.ProductServiceImpl;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -9,7 +8,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import java.time.LocalDate;
 import java.util.List;
 import static org.mockito.Mockito.doReturn;
 import org.junit.jupiter.api.Test;
@@ -28,12 +26,12 @@ class ApplicationTests {
 
 
 	@Test
-	@DisplayName("should display all products in your personally shopping list")
+	@DisplayName("should display all products in your personal shopping list")
 	void getFindAll() {
 
 		String owner = "cosimo.t@live.de";
 
-		Product product = new Product(1,"banane",2,"cosimo.t@live.de",1);
+		Product product = new Product(1,"Banane",2,"cosimo.t@live.de",1);
 		product.setOwner(owner);
 
 		doReturn(List.of(product)).when(productRepository).findAll();
