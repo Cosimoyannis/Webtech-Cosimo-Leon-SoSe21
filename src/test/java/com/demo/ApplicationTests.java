08e66c03c2a4f71fc5e6d1dac617693844fae84b
Cosimoyannis <Cosimo.t@live.de>
2021-06-28 16:40:27 +0200

    1.test

--- src/test/java/com/demo/ApplicationTests.java
+++ src/test/java/com/demo/ApplicationTests.java
@@ -1,13 +1,46 @@
 package com.demo;
-
+import com.example.springboot.Product;
+import com.example.springboot.ProductRepository;
+import com.example.springboot.ProductService;
+import com.example.springboot.ProductServiceImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import java.time.LocalDate;
+import java.util.List;
+import static org.mockito.Mockito.doReturn;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApplicationTests {
 
+	@Autowired
+	private ProductServiceImpl productServiceImpl;
+
+	@MockBean
+	private ProductRepository productRepository;
+
+
+
+
 	@Test
-	void contextLoads() {
+	@DisplayName("should display all products in your personally shopping list")
+	void getFindAll() {
+
+		String owner = "cosimo.t@live.de";
+
+		Product product = new Product(1,"banane",2,"cosimo.t@live.de",1);
+		product.setOwner(owner);
+
+		doReturn(List.of(product)).when(productRepository).findAll();
+		var actual = productServiceImpl.findAll(owner);
+		var expected = product;
+		Assertions.assertSame(expected, actual);
+
 	}
 
 }
